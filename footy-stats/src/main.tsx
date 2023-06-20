5f636662198e5df29839101df32ed40acaa8d359
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2023-06-19 23:46:24 -0600

    feat: migrated app from JavaScript to TypeScript

---
+++ footy-stats/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// eslint-disable-next-line import/no-unresolved
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as Element).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
