6f9cd7c5669d3237c704b2ed7b9a564282980adb
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2023-07-01 21:40:53 -0600

    feat: Added Sticky Footer

--- footy-stats/src/App.tsx
+++ footy-stats/src/App.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import queryClient from "./config";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import queryClient from "./config";
 import { CompetitionWrapper } from "./features/competitions";
 
 // TODO: Change the wrapping of QueryClientProvider
 // TODO: once routes are introduced
 function App() {
   return (
-    <main>
-      <Navbar />
-      <QueryClientProvider client={queryClient}>
-        <CompetitionWrapper />
-      </QueryClientProvider>
+    <main id="app-container">
+      <div id="content-wrapper">
+        <Navbar />
+        <QueryClientProvider client={queryClient}>
+          <CompetitionWrapper />
+        </QueryClientProvider>
+      </div>
+      <Footer />
     </main>
   );
 }
---
+++ footy-stats/src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Created for the passionate{" "}
+        <img src="/ball.gif" width={16} alt="Football logo" /> fans with 💖 by{" "}
+        <a
+          href="https://github.com/sprashanthv-dev"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          sprashanthv
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
