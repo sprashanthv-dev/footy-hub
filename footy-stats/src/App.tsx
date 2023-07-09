569fe9a093a8c73b2ae7d5d86162c3701194639f
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2023-07-08 19:14:05 -0600

    feat: Added Routes and navigation to competition history

--- footy-stats/src/App.tsx
+++ footy-stats/src/App.tsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import queryClient from "./config";
-import { CompetitionWrapper } from "./features/competitions";
+import { CompetitionWrapper, HistoryList } from "./features/competitions";
 
-// TODO: Change the wrapping of QueryClientProvider
-// TODO: once routes are introduced
 function App() {
   return (
     <main id="app-container">
       <div id="content-wrapper">
         <Navbar />
-        <QueryClientProvider client={queryClient}>
-          <CompetitionWrapper />
-        </QueryClientProvider>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route path="/:code" element={<HistoryList />}></Route>
+              <Route path="/" element={<CompetitionWrapper />} />
+            </Routes>
+          </QueryClientProvider>
+        </BrowserRouter>
       </div>
       <Footer />
     </main>
--- footy-stats/src/features/competitions/components/Competition.tsx
+++ footy-stats/src/features/competitions/components/Competition.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { modCompetitionList } from "../models/modCompetitions";
 
@@ -9,21 +10,23 @@ type competition = {
 };
 
 const Competition = ({ info }: competition) => {
-  const { emblem, name: leagueName, area = null } = info;
+  const { emblem, code, name: leagueName, area = null } = info;
 
   const areaName = area ? area.name : "-";
   const areaCode = area ? area.code : "-";
   const flag = area ? area.flag : null;
 
   return (
-    <div className={styles.competition}>
+    <Link to={`/${code}`} className={styles.competition}>
+      {/* <section className={styles.competition}> */}
       <img src={emblem} alt="League Emblem" width={64} />
-      <h2>{leagueName}</h2>
+      <h2 className={styles.leagueName}>{leagueName}</h2>
       <p>
         {areaName} - {areaCode}
       </p>
       {flag && <img src={flag} alt="Country Flag" width={64} />}
-    </div>
+      {/* </section> */}
+    </Link>
   );
 };
 
